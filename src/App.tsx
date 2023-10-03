e34f12870dcb030869fbbe13442dff7f29b32648
seungbeom1999 <khhds00@naver.com>
2023-10-03 22:19:45 +0900

    chore. initial Router settings && initial Style settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./component/Layout";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import Login from "./routes/Login";
+import CreateAccount from "./routes/CreateAccount";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  { path: "/login", element: <Login /> },
+  {
+    path: "/createAccount",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: 'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`;
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/routes/CreateAccount.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateAccount() {
+  return <h1>CreateAccount</h1>;
+}
+
+export default CreateAccount;
